8e0ef42ccb199716f9105c2e4d99bebb3ec24eaf
Prasetyo <prasetyosyifa@gmail.com>
2023-12-07 13:23:39 +0700

    feat(kajian-detail): integrate get presensi

--- src/service/presensi.ts
+++ src/service/presensi.ts
@@ -1,6 +1,6 @@
 import { PostgrestError } from '@supabase/supabase-js';
 import { sbClient } from '../config';
-import { PresensiType, lang } from '../constants';
+import { JamaahType, PresensiType, lang } from '../constants';
 
 export default {
   insertPresensiDB: async (req: PresensiType) => {
@@ -26,5 +26,26 @@ export default {
       .insert([{ kajian_id: req.kajianId, jamaah_id: jamaahId }]);
 
     return { error };
+  },
+
+  getPresensiByKajianIDDB: async (id: string) => {
+    const { data } = await sbClient
+      .from('presensi')
+      .select('*,jamaah (id, name)')
+      .eq('kajian_id', id);
+
+    const resp: JamaahType[] =
+      data?.map((d) => {
+        return {
+          id: d.jamaah?.id,
+          name: d.jamaah?.name,
+          phoneNumber: '',
+          uniqueId: '',
+        };
+      }) || [];
+
+    const count = resp?.length;
+
+    return { resp, count, data };
   },
 };
